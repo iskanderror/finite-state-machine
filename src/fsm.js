18d20bc7ce0cf696bee94904f310a3a6f9b7205c
iskanderror <iskanderror@gmail.com>
2019-10-17 21:39:14 +0500

    feat: finite-state machine

--- src/fsm.js
+++ src/fsm.js
@@ -1,32 +1,66 @@
 class FSM {
-    /**
-     * Creates new FSM instance.
-     * @param config
-     */
-    constructor(config) {}
+  /**
+   * Creates new FSM instance.
+   * @param config
+   */
+  constructor(config) {
+    this._config = config;
+    if(this._config===null || this._config===undefined){
+      throw new Error("Config is not defined");
+    }
+    this._state = this._config['initial'];
+    this._stateHistory = [];
+    this._stateRedo = [];
+  }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+      return this._state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+      let statesList = Object.keys(this._config['states']);
+      if (!statesList.includes(state)){
+        throw new Error ('No such state');
+      }
+      this._stateHistory.push(this._state);
+      this._stateRedo.length = 0;
+      this._state = state;
+      return this;
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+      let currentState = this._config['states'][this._state];
+      let eventsList = Object.keys(currentState['transitions']);
+      if (!eventsList.includes(event)){
+        throw new Error ('No such event for current state');
+      }
+      this._stateHistory.push(this._state);
+      this._stateRedo.length = 0;
+      this._state = currentState['transitions'][event];
+      return this;
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+      this._stateHistory.push(this._state);
+      this._stateRedo.length = 0;
+      this._state = this._config['initial'];
+      return this;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,27 +68,86 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+      let statesObj = this._config['states'];
+      let statesList = Object.keys(statesObj);
+      if (event === undefined) {
+        return statesList;
+      }
+      let filteredStatesList = statesList.filter(
+        item => Object.keys(statesObj[item]['transitions']).includes(event)
+        );
+      return filteredStatesList;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+      if (this._stateHistory.length == 0){
+        return false;
+      }
+      this._stateRedo.push(this._state);
+      this._state = this._stateHistory.pop();
+      return true;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+      if (this._stateRedo.length == 0){
+        return false;
+      }
+      this._stateHistory.push(this._state);
+      this._state = this._stateRedo.pop();
+      return true;
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+      this._stateHistory.length = 0;
+      this._stateRedo.length = 0;
+    }
 }
+/*
+const config = {
+  initial: 'normal',
+  states: {
+      normal: {
+          transitions: {
+              study: 'busy',
+          }
+      },
+      busy: {
+          transitions: {
+              get_tired: 'sleeping',
+              get_hungry: 'hungry',
+          }
+      },
+      hungry: {
+          transitions: {
+              eat: 'normal'
+          },
+      },
+      sleeping: {
+          transitions: {
+              get_hungry: 'hungry',
+              get_up: 'normal',
+          },
+      },
+  }
+};
+const student = new FSM(config);
+
+student.getStates('get_hungry');
+*/
 
 module.exports = FSM;
 
